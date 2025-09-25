3e661ee9fedf575dcdbdc37e9fdb3f43d3443518
AlejandroSierraUV26 <sierra.alejandro@correounivalle.edu.co>
2025-09-25 15:35:28 -0500

    feat(users): add initial CRUD structure for users

--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from '../service/users.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { LoginUserDto } from '../dto/login-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  registerWithEmail(@Body() dto: CreateUserDto) {
+    return this.usersService.registerWithEmail(dto);
+  }
+
+  @Post('login')
+  loginWithEmail(@Body() dto: LoginUserDto) {
+    return this.usersService.loginWithEmail(dto);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return this.usersService.findById(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    return this.usersService.deleteUser(id);
+  }
+}
